61e6e01177bef0eb1df2c76f39ca4ab5a7f5aa05
tobidelly <tobidelly@gmail.com>
2024-01-16 15:57:16 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0:
-        diff = 0
-    else:
-        diff = 32
-        print('{}'.format(chr(i - diff)), end='')
+    print('{}'.format(chr(i) + chr(i - 32) if i % 2 == 0 else chr(i - 32)), end='')
