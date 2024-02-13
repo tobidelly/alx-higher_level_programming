d5b72fb24459131db90ef896251611af4181e879
tobidelly <tobidelly@gmail.com>
2024-02-13 11:19:49 -0800

    3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""SayMyName module"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Args: Prints My name is <first name> <last name>.
+        :param last_name: <first name> (must be strings),
+        :param first_name: <last name> (must be strings).
+
+    Raises:
+    TypeError - if not a string.
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
