7690cbda7303a32fe92dccfdbfd28ec8b05add0f
tobidelly <tobidelly@gmail.com>
2024-02-13 11:35:26 -0800

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Text indentation"""
+
+
+def text_indentation(text):
+    """
+    Args: Prints a text with 2 new lines after each of these
+    character . ? and :
+    There should be no space at the beginning or at the end of
+    each printed line.
+
+        :param text: the text to print (a string).
+
+    Raises: TypeError - If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    count = 0
+    while count < len(text) and text[count] == " ":
+        count = count + 1
+
+    while count < len(text):
+        print(text[count], end="")
+        if text[count] == "\n" or text[count] in ".?:":
+            if text[count] in ".?:":
+                print("\n")
+            count = count + 1
+            while count < len(text) and text[count] == " ":
+                count = count + 1
+            continue
+        count = count + 1
