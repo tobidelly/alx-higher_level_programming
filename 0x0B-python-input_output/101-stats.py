8cb33159df5d1f98b5e5cee79543da71c1f09cc7
tobidelly <tobidelly@gmail.com>
2024-02-20 07:01:26 -0800

    101-stats.py

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+reads stdin line by line and computes metrics
+"""
+import sys
+
+file_size = 0
+status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+i = 0
+try:
+    for line in sys.stdin:
+        tokens = line.split()
+
+        if len(tokens) >= 2:
+            a = i
+            if tokens[-2] in status_tally:
+                status_tally[tokens[-2]] += 1
+                i += 1
+            try:
+                file_size += int(tokens[-1])
+                if a == i:
+                    i += 1
+            except FileNotFoundError:
+                if a == i:
+                    continue
+
+        if i % 10 == 0:
+            print(f"File size: {file_size}")
+            for key, value in sorted(status_tally.items()):
+                if value:
+                    print(f"{key}: {value}")
+    print(f"File size: {file_size}")
+
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print(f"{key}: {value}")
+
+except KeyboardInterrupt:
+    print(f"File size: {file_size}")
+
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print(f"{key}: {value}")
